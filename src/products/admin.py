c91960ff3a87731a10a6fff76e22663e96110ab4
gerardo <gerardo.saldivar.10@gmail.com>
2019-06-13 02:49:34 -0500

    add modelos

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from .models import Actividad
+from .models import Pratrocinador
+from .models import Contacto
+from .models import Ponente
+from .models import Entrada
 
 admin.site.register(Actividad)
+admin.site.register(Pratrocinador)
+admin.site.register(Contacto)
+admin.site.register(Ponente)
+admin.site.register(Entrada)
--- src/products/models.py
+++ src/products/models.py
@@ -8,3 +8,35 @@ class Actividad(models.Model):
 
     def get_absolute_url(self):
         return reverse("products:actividad-detail", kwargs={"id": self.id}) #f"/products/{self.id}/"
+
+class Pratrocinador(models.Model):
+    nombre      = models.CharField(max_length=120) # max_length = required
+    imagen      = models.CharField(max_length=120)
+    logotipo    = models.CharField(max_length=120)
+
+    def __str__(self):
+    	return self.nombre
+
+class Contacto(models.Model):
+	nombre      = models.CharField(max_length=120) # max_length = required
+	correo      = models.CharField(max_length=120)	
+
+	def __str__(self):
+		return self.nombre
+
+class Ponente(models.Model):
+	nombre      = models.CharField(max_length=120) # max_length = required
+	tema        = models.CharField(max_length=120)	
+
+	def __str__(self):
+		return self.nombre
+
+class Entrada(models.Model):
+	nombre      = models.CharField(max_length=120) # max_length = required
+	correo      = models.CharField(max_length=120)
+	ocupacion   = models.CharField(max_length=120)
+	telefono    = models.CharField(max_length=120)
+	tipo        = models.CharField(max_length=120)
+
+	def __str__(self):
+		return self.nombre
