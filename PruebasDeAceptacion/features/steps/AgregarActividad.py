fe66a4024433622c48a1554200293ff7ccb06e94
ArianaRodriguezM <arimaromo@hotmail.com>
2019-06-13 06:11:32 -0500

    agregaract

--- PruebasDeAceptacion/features/steps/AgregarActividad.py
+++ PruebasDeAceptacion/features/steps/AgregarActividad.py
@@ -1,34 +1,42 @@
+from behave import given, when, then
+import time
+
 @given(u'Que presiono "Actividad"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Que presiono "Actividad"')
+    context.browser.get('http://localhost:8000/admin')
+    context.browser.find_element_by_xpath("/html/body/div/div[2]/div[1]/div[2]/table/tbody/tr[1]/th/a").click()
+    
 
 
 @when(u'doy click en el bot▒n agregar')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When doy click en el bot▒n agregar')
+    context.browser.find_element_by_xpath("/html/body/div/div[3]/div/ul/li/a")
+    
 
-@when(u'ingreso "act1" en el campo nombre')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When ingreso "act1" en el campo nombre')
+@when(u'ingreso "{nombre_act}" en el campo nombre')
+def step_impl(context, nombre_act):
+    context.browser.find_element_by_xpath(//*[@id="id_nombre"]).send_keys(nombre_act)
+
+@when(u'la hora "{hora}" en el campo hora')
+def step_impl(context, hora):
+    context.browser.find_element_by_xpath(//*[@id="id_hora"]).send_keys(hora)
 
 
-@when(u'la hora "08:00" en el campo hora')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When la hora "08:00" en el campo hora')
-
-
-@when(u'ponente "ponente1" en el campo ponente')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When ponente "ponente1" en el campo ponente')
-
+@when(u'ponente "{ponente}" en el campo ponente')
+def step_impl(context, ponente):
+    context.browser.find_element_by_xpath(//*[@id="id_encargado"]).send_keys(ponente)
+    
 
 @when(u'doy click en el boton agregar')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When doy click en el boton agregar')
+    context.browser.find_element_by_xpath('/html/body/div/div[3]/div/form/div/div/input[1]').click()
+    
 
-
-@then(u'Puedo ver el mensaje de registro de actividad "La actividad se agreg▒ exitosamente"')
-def step_impl(context):
+@then(u'Puedo ver el mensaje de registro de actividad "{mensaje}}"')
+def step_impl(context,mensaje):
+    exito = context.browser.find_element_by_xpath('/html/body/div/ul/li')
+    assert exito.text in mensaje, \
+    "Obtenido " + exito.text + " y esperado " + mensaje
     raise NotImplementedError(u'STEP: Then Puedo ver el mensaje de registro de actividad "La actividad se agreg▒ exitosamente"')
 
 
