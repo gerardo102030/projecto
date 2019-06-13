e40ce87b0d1bf7a1b94d3ec6d4821b076dedfdba
gerardo <gerardo.saldivar.10@gmail.com>
2019-06-13 03:42:06 -0500

    steps

---
+++ PruebasDeAceptacion/features/steps/EliminarActividad.py
@@ -0,0 +1,28 @@
+@given(u'Que presiono "Actividad"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given Que presiono "Actividad"')
+
+
+@given(u'presiono sobre la activad "act1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given presiono sobre la activad "act1"')
+
+
+@when(u'doy click en el bot▒n eliminar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When doy click en el bot▒n eliminar')
+
+
+@when(u'doy click en aceptar en el mensaje de confirmaci▒n de eliminaci▒n')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When doy click en aceptar en el mensaje de confirmaci▒n de eliminaci▒n')
+
+
+@then(u'puedo ver el mensaje de eliminaci▒n de activad "La actividad "act1" se ha eliminado correctamente')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then puedo ver el mensaje de eliminaci▒n de activad "La actividad "act1" se ha eliminado correctamente')
+
+
+@then(u'puedo ver el mensaje de error de eliminaci▒n de activad "La actividad "act1" no pudo ser eliminada"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then puedo ver el mensaje de error de eliminaci▒n de activad "La actividad "act1" no pudo ser eliminada"')
---
+++ PruebasDeAceptacion/features/steps/ModificarActividad.py
@@ -0,0 +1,43 @@
+@given(u'Que presiono "Actividad"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given Que presiono "Actividad"')
+
+
+@when(u'doy click en el bot▒n modificar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When doy click en el bot▒n modificar')
+
+
+@when(u'doy click sobre la activad "act1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When doy click sobre la activad "act1"')
+
+
+@when(u'puedo ver el formulario para agregar la actividad')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When puedo ver el formulario para agregar la actividad')
+
+
+@when(u'cambio el nombre por "act2" en el campo nombre')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When cambio el nombre por "act2" en el campo nombre')
+
+
+@when(u'doy click en el boton guardar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When doy click en el boton guardar')
+
+
+@then(u'Puedo ver el mensaje de modificacion de actividad "La informaci▒n se actualiz▒ correctamente"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Puedo ver el mensaje de modificacion de actividad "La informaci▒n se actualiz▒ correctamente"')
+
+
+@when(u'cambio el nombre por "!"#$$" en el campo nombre')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When cambio el nombre por "!"#$$" en el campo nombre')
+
+
+@then(u'Puedo ver el mensaje de error de modificacion de actividad "El formato de nombre no es el correcto. No se admiten caracteres especiales"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Puedo ver el mensaje de error de modificacion de actividad "El formato de nombre no es el correcto. No se admiten caracteres especiales"')
